1a12ab528b2030ce4bbfd79230c865bccc2bf34c
Ihor Mikheyev <ihor.mikheiev@gmail.com>
2025-02-23 20:18:59 +0100

    Fixed Gallery section, change button tag to a
    Fix a path to first slide img;
    Added link and hover for this link
    Change buton tag to a tag, wrote location and added target

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,7 @@
 const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
