5e602d73247c955dd03b105cd3e9fe2dd6a4e10d
Ihor Mikheyev <ihor.mikheiev@gmail.com>
2025-02-26 14:56:21 +0100

    Fixed swiper gallery
    Add breakpoint for desktop screen. Changed count of images for show with both breakpoints.

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,10 +1,19 @@
 const swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
+  slidesPerView: 1,
+  spaceBetween: 24,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3, // Кількість слайдів на десктопі
+      spaceBetween: 24,
+    },
   },
 });
 
